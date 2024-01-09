1f19b8957cb44330285439474bc168edda3e5155
Cristopher Delgado <delgadocristopher524@gmail.com>
2024-01-08 22:45:03 -0800

    edited call back to be 0.001 diff

--- functions.py
+++ functions.py
@@ -103,7 +103,7 @@ def get_callbacks():
     from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
     
     # Initialize callbacks 
-    stop = EarlyStopping(monitor='val_loss', mode='min', min_delta=0.01, patience=8, restore_best_weights=True, verbose=1)
+    stop = EarlyStopping(monitor='val_loss', mode='min', min_delta=0.001, patience=8, restore_best_weights=True, verbose=1)
     #reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, min_delta=0.01, patience=4, verbose=1)
     
     # Initialize callbacks 
